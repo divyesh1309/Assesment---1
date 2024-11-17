58c84d4e3ea7d3844f867a5f16a0477e393b8414
Divyesh <divyeshchudasama578@gmail.com>
2024-11-17 11:24:25 +0530

    Add necessary commente

--- Asse.c
+++ Asse.c
@@ -6,19 +6,21 @@ void main()
 
     printf("    <<----->>menu<<----->>");
 
+
+    //we used (do while) beacuse ek baar menu dikhake input le , or extra order cahiye to loop dubara  chalega 
     do 
     {
         printf("\n1. Pizza    price = 180rs/pcs");
         printf("\n2. Burger   price = 100rs/pcs");
         printf("\n3. Dosa     price = 120rs/pcs");
         printf("\n4. Idli     price = 50rs/pcs");
-        printf("\nPlease Enter your choice : ");
+        printf("\nPlease Enter your choice : "); //kon kon si choice chahiye 
         scanf("%d", &choice);
 
-        printf("Enter the quantity : ");
+        printf("Enter the quantity : ");   //kitni quntity me chahiye
         scanf("%d", &quantity);
 
-
+        //use switch case for user ki konsi choice he or kitni quantity me he uska total amount add karga 
         switch (choice) 
         {
             case 1: total_amount += 180 * quantity;
@@ -29,16 +31,19 @@ void main()
              break;
             case 4: total_amount += 50 * quantity;
              break;
-            default: printf("\nYour choice are invelid!"); continue;
+            default: printf("\nYour choice are invelid!");  //user galt choice choose kare is liye
+             continue;   //dubara input de sake 
+
+
         }
 
-        printf("\nTotal Amount is = %d", total_amount);
-        printf("\nDo you want to place more orders? (y/n) : ");
+        printf("\nTotal Amount is = %d", total_amount);  
+        printf("\nDo you want to place more orders? (y/n) : ");    //more order ke liye ('y','n') 
         scanf(" %c", &more_orders);
     } 
 
-    while (more_orders == 'y' || more_orders == 'y');
+    while (more_orders == 'y' || more_orders == 'y');      // 'y', 'Y' user input karega to loop dubara chalega    
 
-    printf("\nThank you! Your final bill is :- %d", total_amount);
+    printf("\nThank you! Your final bill is :- %d", total_amount);  //'n' , 'N' user input karega to loop close hoga or fainal bill print kar dega   
     
 }
